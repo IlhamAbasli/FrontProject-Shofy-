938d21e47a0c8fffea6cf12a5ccae1fd06827bce
IlhamAbaslı <ilhamra@code.edu.az>
2024-03-20 02:59:34 +0400

    home page slider area added

--- assets/js/home.js
+++ assets/js/home.js
@@ -9,4 +9,20 @@ $(function () {
     $(this).next().toggleClass("menu-down");
     $(this).parent().siblings().find(".menu").removeClass("menu-down");
   });
+
+
+  $(".all-categories").click(function(){
+    $(".categories").toggleClass("d-none")
+  })
+
+
+  $(".products-nav").mouseenter(function(){
+    $(".submenu").addClass("submenu-show");
+    $(".submenu").removeClass("submenu-close");
+  })
+
+  $(".products-nav").mouseleave(function(){
+    $(".submenu").removeClass("submenu-show");
+    $(".submenu").addClass("submenu-close");
+  })
 });
