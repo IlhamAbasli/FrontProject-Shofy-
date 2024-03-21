12b98c86a912096ad92615190e02dd98a5fbfd34
IlhamAbaslı <ilhamra@code.edu.az>
2024-03-21 21:24:17 +0400

    bottom slider added to home page,new arrivals added,specials added

--- assets/js/home.js
+++ assets/js/home.js
@@ -10,120 +10,133 @@ $(function () {
     $(this).parent().siblings().find(".menu").removeClass("menu-down");
   });
 
+  $(".all-categories").click(function () {
+    $(".categories").toggleClass("d-none");
+  });
 
-  $(".all-categories").click(function(){
-    $(".categories").toggleClass("d-none")
-  })
-
-
-  $(".products-nav").mouseenter(function(){
+  $(".products-nav").mouseenter(function () {
     $(".submenu").addClass("submenu-show");
     $(".submenu").removeClass("submenu-close");
-  })
+  });
 
-  $(".products-nav").mouseleave(function(){
+  $(".products-nav").mouseleave(function () {
     $(".submenu").removeClass("submenu-show");
     $(".submenu").addClass("submenu-close");
-  })
+  });
 
+  //#region back-to-top-button
+  var btn = $("#button");
 
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+      btn.addClass("show");
+    } else {
+      btn.removeClass("show");
+    }
+  });
+
+  btn.on("click", function (e) {
+    e.preventDefault();
+    $("html, body").animate({ scrollTop: 0 }, "300");
+  });
+
+  //#endregion
 
   //#region slider-area
-  
-  $("#slider-area .controllers .left").click(function(event){
+
+  $("#slider-area .controllers .left").click(function (event) {
     event.preventDefault();
     let activeSlider = $(".active-slider");
     let activeSliderImage = $(".active-slider-image");
     let dotActive = $(".active-dot");
 
+    if (
+      activeSlider.prev().length == 0 &&
+      activeSliderImage.prev().length == 0 &&
+      dotActive.next().length == 0
+    ) {
+      return;
+    } else {
+      activeSlider.removeClass("active-slider");
+      activeSliderImage.removeClass("active-slider-image");
+      dotActive.removeClass("active-dot");
 
-    if(activeSlider.prev().length == 0 && activeSliderImage.prev().length == 0 && dotActive.next().length == 0){
-        return;
-      }else{
-        activeSlider.removeClass("active-slider");
-        activeSliderImage.removeClass("active-slider-image");
-        dotActive.removeClass("active-dot");
-    
-        activeSlider.prev().addClass("active-slider");
-        activeSliderImage.prev().addClass("active-slider-image");
-        dotActive.prev().addClass("active-dot");
-      }
+      activeSlider.prev().addClass("active-slider");
+      activeSliderImage.prev().addClass("active-slider-image");
+      dotActive.prev().addClass("active-dot");
+    }
+  });
 
-  })
-
-  $("#slider-area .controllers .right").click(function(event){
+  $("#slider-area .controllers .right").click(function (event) {
     event.preventDefault();
     let activeSlider = $(".active-slider");
     let activeSliderImage = $(".active-slider-image");
     let dotActive = $(".active-dot");
 
-    if(activeSlider.next().length == 0 && activeSliderImage.next().length == 0 && dotActive.next().length == 0){
-        return;
-      }else{
-        activeSlider.removeClass("active-slider");
-        activeSliderImage.removeClass("active-slider-image");
-        dotActive.removeClass("active-dot");
-    
-        activeSlider.next().addClass("active-slider");
-        activeSliderImage.next().addClass("active-slider-image");
-        dotActive.next().addClass("active-dot");
-      }
-  })
+    if (
+      activeSlider.next().length == 0 &&
+      activeSliderImage.next().length == 0 &&
+      dotActive.next().length == 0
+    ) {
+      return;
+    } else {
+      activeSlider.removeClass("active-slider");
+      activeSliderImage.removeClass("active-slider-image");
+      dotActive.removeClass("active-dot");
 
-  $("#slider-area .pagination .first-slide-dot").click(function(){
+      activeSlider.next().addClass("active-slider");
+      activeSliderImage.next().addClass("active-slider-image");
+      dotActive.next().addClass("active-dot");
+    }
+  });
+
+  $("#slider-area .pagination .first-slide-dot").click(function () {
     $(this).addClass("active-dot");
     $(this).siblings().removeClass("active-dot");
     $(".first-slide").addClass("active-slider");
     $(".first-slide").siblings().removeClass("active-slider");
     $(".first-slider-image").addClass("active-slider-image");
     $(".first-slider-image").siblings().removeClass("active-slider-image");
-  })
+  });
 
-
-
-  $("#slider-area .pagination .second-slide-dot").click(function(){
+  $("#slider-area .pagination .second-slide-dot").click(function () {
     $(this).addClass("active-dot");
     $(this).siblings().removeClass("active-dot");
     $(".second-slide").addClass("active-slider");
     $(".second-slide").siblings().removeClass("active-slider");
     $(".second-slider-image").addClass("active-slider-image");
     $(".second-slider-image").siblings().removeClass("active-slider-image");
-  })
+  });
 
-
-  $("#slider-area .pagination .third-slide-dot").click(function(){
+  $("#slider-area .pagination .third-slide-dot").click(function () {
     $(this).addClass("active-dot");
     $(this).siblings().removeClass("active-dot");
     $(".third-slide").addClass("active-slider");
     $(".third-slide").siblings().removeClass("active-slider");
     $(".third-slider-image").addClass("active-slider-image");
     $(".third-slider-image").siblings().removeClass("active-slider-image");
-  })
-
-
+  });
 
   //#endregion
-  
 
   //#region sidebar
-  $(".bars").click(function(e){
+  $(".bars").click(function (e) {
     e.preventDefault();
     $(".right-side-menu").addClass("opened");
     $(".right-side-menu").removeClass("closed");
-  })
+  });
 
-  $(".close").click(function(e){
+  $(".close").click(function (e) {
     e.preventDefault();
     $(".right-side-menu").removeClass("opened");
     $(".right-side-menu").addClass("closed");
-  })
-  
-  //#endregion
+  });
 
+  //#endregion
 
   //#region product-tabmenu
 
-  $("#products .tab-categories .new-products").click(function(e){
+  $("#products .tab-categories .new-products").click(function (e) {
     e.preventDefault();
     $(this).addClass("active");
     $("#products .tab-categories .featured-products").removeClass("active");
@@ -131,10 +144,9 @@ $(function () {
     $("#products .new-products-list").removeClass("d-none");
     $("#products .featured-products-list").addClass("d-none");
     $("#products .top-products-list").addClass("d-none");
-  })
+  });
 
-
-  $("#products .tab-categories .featured-products").click(function(e){
+  $("#products .tab-categories .featured-products").click(function (e) {
     e.preventDefault();
     $(this).addClass("active");
     $("#products .tab-categories .new-products").removeClass("active");
@@ -142,10 +154,9 @@ $(function () {
     $("#products .new-products-list").addClass("d-none");
     $("#products .featured-products-list").removeClass("d-none");
     $("#products .top-products-list").addClass("d-none");
-  })
+  });
 
-
-  $("#products .tab-categories .top-products").click(function(e){
+  $("#products .tab-categories .top-products").click(function (e) {
     e.preventDefault();
     $(this).addClass("active");
     $("#products .tab-categories .featured-products").removeClass("active");
@@ -153,35 +164,30 @@ $(function () {
     $("#products .new-products-list").addClass("d-none");
     $("#products .featured-products-list").addClass("d-none");
     $("#products .top-products-list").removeClass("d-none");
-  })
-
+  });
 
   //#endregion
-  
-
 
   //#region add-to-cart&wishlist
-  
-  let products = [];
-  let wishlist = []
 
-  if(JSON.parse(localStorage.getItem("products")) == null){
-    localStorage.setItem("products",JSON.stringify(products));
-  }else{
+  let products = [];
+  let wishlist = [];
+
+  if (JSON.parse(localStorage.getItem("products")) == null) {
+    localStorage.setItem("products", JSON.stringify(products));
+  } else {
     products = JSON.parse(localStorage.getItem("products"));
   }
 
-
-  if(JSON.parse(localStorage.getItem("wishlist")) == null){
-    localStorage.setItem("wishlist",JSON.stringify(wishlist));
-  }else{
+  if (JSON.parse(localStorage.getItem("wishlist")) == null) {
+    localStorage.setItem("wishlist", JSON.stringify(wishlist));
+  } else {
     wishlist = JSON.parse(localStorage.getItem("wishlist"));
   }
 
-
   function getBasketCount(products) {
     let count = 0;
-    if(products.length != 0){
+    if (products.length != 0) {
       for (const product of products) {
         count += product.count;
       }
@@ -192,7 +198,7 @@ $(function () {
 
   function getWishlistCount(products) {
     let count = 0;
-    if(wishlist.length != 0){
+    if (wishlist.length != 0) {
       for (const product of products) {
         count++;
       }
@@ -201,159 +207,268 @@ $(function () {
     $("#header-mid .actions .wishlist-count").html(count);
   }
 
-  function checkWishlist(wishlist){
+  function checkWishlist(wishlist) {
     let products = $(".product");
     for (const product of products) {
       for (const item of wishlist) {
-        if(product.getAttribute("data-id") == item.id){
-          product.firstElementChild.firstElementChild.nextElementSibling.firstElementChild.lastElementChild.firstElementChild.setAttribute("class","add-to-wishlist active-wishlist");
-
+        if (product.getAttribute("data-id") == item.id) {
+          product.firstElementChild.firstElementChild.nextElementSibling.firstElementChild.lastElementChild.firstElementChild.setAttribute(
+            "class",
+            "add-to-wishlist active-wishlist"
+          );
         }
       }
     }
   }
 
-  checkWishlist(wishlist)
-  getBasketCount(products); 
+  checkWishlist(wishlist);
+  getBasketCount(products);
   getWishlistCount(wishlist);
 
-  $(".add-to-cart").click(function(e){
+  $(".add-to-cart").click(function (e) {
     e.preventDefault();
-    let productId = $(this).parent().parent().parent().parent().parent().attr("data-id");
+    let productId = $(this)
+      .parent()
+      .parent()
+      .parent()
+      .parent()
+      .parent()
+      .attr("data-id");
     let productImage = $(this).parent().parent().parent().prev().attr("src");
-    let productName = $(this).parent().parent().parent().parent().next().children().first().next().html();
-    let productPrice = $(this).parent().parent().parent().parent().next().children().last().children().last().children().first().html();
+    let productName = $(this)
+      .parent()
+      .parent()
+      .parent()
+      .parent()
+      .next()
+      .children()
+      .first()
+      .next()
+      .html();
+    let productPrice = $(this)
+      .parent()
+      .parent()
+      .parent()
+      .parent()
+      .next()
+      .children()
+      .last()
+      .children()
+      .last()
+      .children()
+      .first()
+      .html();
 
-
-    if($(this).parent().parent().parent().parent().parent().hasClass("out-of-stock")){
-      toastr["error"](`${productName} out of stock`)
+    if (
+      $(this)
+        .parent()
+        .parent()
+        .parent()
+        .parent()
+        .parent()
+        .hasClass("out-of-stock")
+    ) {
+      toastr["error"](`${productName} out of stock`);
       toastr.options = {
-        "closeButton": false,
-        "debug": false,
-        "newestOnTop": false,
-        "progressBar": false,
-        "positionClass": "toast-top-center",
-        "preventDuplicates": false,
-        "onclick": null,
-        "showDuration": "300",
-        "hideDuration": "1000",
-        "timeOut": "5000",
-        "extendedTimeOut": "1000",
-        "showEasing": "swing",
-        "hideEasing": "linear",
-        "showMethod": "fadeIn",
-        "hideMethod": "fadeOut"
-      }
+        closeButton: false,
+        debug: false,
+        newestOnTop: false,
+        progressBar: false,
+        positionClass: "toast-top-center",
+        preventDuplicates: false,
+        onclick: null,
+        showDuration: "300",
+        hideDuration: "1000",
+        timeOut: "5000",
+        extendedTimeOut: "1000",
+        showEasing: "swing",
+        hideEasing: "linear",
+        showMethod: "fadeIn",
+        hideMethod: "fadeOut",
+      };
       return;
     }
 
+    let existProduct = products.find((m) => m.id == parseInt(productId));
 
-    let existProduct = products.find(m => m.id == parseInt(productId))
-    
-    if(existProduct != undefined){
+    if (existProduct != undefined) {
       existProduct.count++;
-    }else{
+    } else {
       products.push({
         id: parseInt(productId),
         image: productImage,
         name: productName,
         price: parseFloat(productPrice),
-        count: 1
-      })
+        count: 1,
+      });
     }
 
-    localStorage.setItem("products",JSON.stringify(products));
+    localStorage.setItem("products", JSON.stringify(products));
 
-    toastr["success"](`${productName} added to cart`)
+    toastr["success"](`${productName} added to cart`);
     toastr.options = {
-      "closeButton": false,
-      "debug": false,
-      "newestOnTop": false,
-      "progressBar": false,
-      "positionClass": "toast-top-center",
-      "preventDuplicates": false,
-      "onclick": null,
-      "showDuration": "300",
-      "hideDuration": "1000",
-      "timeOut": "5000",
-      "extendedTimeOut": "1000",
-      "showEasing": "swing",
-      "hideEasing": "linear",
-      "showMethod": "fadeIn",
-      "hideMethod": "fadeOut"
-    }
+      closeButton: false,
+      debug: false,
+      newestOnTop: false,
+      progressBar: false,
+      positionClass: "toast-top-center",
+      preventDuplicates: false,
+      onclick: null,
+      showDuration: "300",
+      hideDuration: "1000",
+      timeOut: "5000",
+      extendedTimeOut: "1000",
+      showEasing: "swing",
+      hideEasing: "linear",
+      showMethod: "fadeIn",
+      hideMethod: "fadeOut",
+    };
 
     getBasketCount(products);
-  })
+  });
 
-  $(".add-to-wishlist").click(function(e){
+  $(".add-to-wishlist").click(function (e) {
     e.preventDefault();
-    let productId = $(this).parent().parent().parent().parent().parent().attr("data-id");
+    let productId = $(this)
+      .parent()
+      .parent()
+      .parent()
+      .parent()
+      .parent()
+      .attr("data-id");
     let productImage = $(this).parent().parent().parent().prev().attr("src");
-    let productName = $(this).parent().parent().parent().parent().next().children().first().next().html();
-    let productPrice = $(this).parent().parent().parent().parent().next().children().last().children().last().children().first().html();
+    let productName = $(this)
+      .parent()
+      .parent()
+      .parent()
+      .parent()
+      .next()
+      .children()
+      .first()
+      .next()
+      .html();
+    let productPrice = $(this)
+      .parent()
+      .parent()
+      .parent()
+      .parent()
+      .next()
+      .children()
+      .last()
+      .children()
+      .last()
+      .children()
+      .first()
+      .html();
 
-    let existProduct = wishlist.find(m => m.id == parseInt(productId))
-    
-    if(existProduct != undefined){
-      toastr["error"](`${productName} remove to wishlist`)
-    toastr.options = {
-      "closeButton": false,
-      "debug": false,
-      "newestOnTop": false,
-      "progressBar": false,
-      "positionClass": "toast-top-center",
-      "preventDuplicates": false,
-      "onclick": null,
-      "showDuration": "300",
-      "hideDuration": "1000",
-      "timeOut": "5000",
-      "extendedTimeOut": "1000",
-      "showEasing": "swing",
-      "hideEasing": "linear",
-      "showMethod": "fadeIn",
-      "hideMethod": "fadeOut"
-    }
-    return;
-    }else{
+    let existProduct = wishlist.find((m) => m.id == parseInt(productId));
+
+    if (existProduct != undefined) {
+      toastr["error"](`${productName} remove to wishlist`);
+      toastr.options = {
+        closeButton: false,
+        debug: false,
+        newestOnTop: false,
+        progressBar: false,
+        positionClass: "toast-top-center",
+        preventDuplicates: false,
+        onclick: null,
+        showDuration: "300",
+        hideDuration: "1000",
+        timeOut: "5000",
+        extendedTimeOut: "1000",
+        showEasing: "swing",
+        hideEasing: "linear",
+        showMethod: "fadeIn",
+        hideMethod: "fadeOut",
+      };
+      return;
+    } else {
       wishlist.push({
         id: parseInt(productId),
         image: productImage,
         name: productName,
         price: parseFloat(productPrice),
-        count: 1
-      })
+        count: 1,
+      });
       $(this).addClass("active-wishlist");
     }
-    
-    
-    localStorage.setItem("wishlist",JSON.stringify(wishlist));
 
-    toastr["success"](`${productName} added to wishlist`)
+    localStorage.setItem("wishlist", JSON.stringify(wishlist));
+
+    toastr["success"](`${productName} added to wishlist`);
     toastr.options = {
-      "closeButton": false,
-      "debug": false,
-      "newestOnTop": false,
-      "progressBar": false,
-      "positionClass": "toast-top-center",
-      "preventDuplicates": false,
-      "onclick": null,
-      "showDuration": "300",
-      "hideDuration": "1000",
-      "timeOut": "5000",
-      "extendedTimeOut": "1000",
-      "showEasing": "swing",
-      "hideEasing": "linear",
-      "showMethod": "fadeIn",
-      "hideMethod": "fadeOut"
-    }
+      closeButton: false,
+      debug: false,
+      newestOnTop: false,
+      progressBar: false,
+      positionClass: "toast-top-center",
+      preventDuplicates: false,
+      onclick: null,
+      showDuration: "300",
+      hideDuration: "1000",
+      timeOut: "5000",
+      extendedTimeOut: "1000",
+      showEasing: "swing",
+      hideEasing: "linear",
+      showMethod: "fadeIn",
+      hideMethod: "fadeOut",
+    };
 
     getWishlistCount(wishlist);
-  })
-
+  });
 
   //#endregion
-  
-  
-  
+
+  //#region tablet-slider
+
+  $("#tablet-collection .pagination .first-slide-dot").click(function () {
+    $(this).addClass("active-dot");
+    $(this).siblings().removeClass("active-dot");
+    $(".first-slide").addClass("active-slider");
+    $(".first-slide").siblings().removeClass("active-slider");
+    $(".first-slider-image").addClass("active-slider-image");
+    $(".first-slider-image").siblings().removeClass("active-slider-image");
+  });
+
+  $("#tablet-collection .pagination .second-slide-dot").click(function () {
+    $(this).addClass("active-dot");
+    $(this).siblings().removeClass("active-dot");
+    $(".second-slide").addClass("active-slider");
+    $(".second-slide").siblings().removeClass("active-slider");
+    $(".second-slider-image").addClass("active-slider-image");
+    $(".second-slider-image").siblings().removeClass("active-slider-image");
+  });
+
+  $("#tablet-collection .pagination .third-slide-dot").click(function () {
+    $(this).addClass("active-dot");
+    $(this).siblings().removeClass("active-dot");
+    $(".third-slide").addClass("active-slider");
+    $(".third-slide").siblings().removeClass("active-slider");
+    $(".third-slider-image").addClass("active-slider-image");
+    $(".third-slider-image").siblings().removeClass("active-slider-image");
+  });
+
+  //#endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
 });
